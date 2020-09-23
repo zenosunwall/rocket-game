0a7f095950552c8ec2f9e65aa7441069bfb850dd
Zebulun Wilson <zebulun.wilson@gmail.com>
2020-09-22 21:47:18 -0400

    Orginized and redesigned Rocket

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Thrusing");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating Right");
+        }
+    }
+}
